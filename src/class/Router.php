0b6a7dc98630822c64b08c0d56e2c56ee4bad73c
bulton-fr <bulton.fr@gmail.com>
2017-04-22 19:59:48 +0200

    Add the method (and the call to it) that uses observer to declare that the route was found

--- src/class/Router.php
+++ src/class/Router.php
@@ -118,6 +118,8 @@ class Router
         //Add gets datas in route to $_GET var
         $this->addDatasToGetAndPostVar($routeInfo[1]);
         $this->addToSuperglobalVar('GET', $routeInfo[2]);
+        
+        $this->sendNotifyRouteFindToOthers();
     }
     
     /**
@@ -186,5 +188,18 @@ class Router
         if (isset($routeInfos['get'])) {
             $this->addToSuperglobalVar('GET', $routeInfos['get']);
         }
+    }
+    
+    /**
+     * Send to all observer of Application a notify who contains the message
+     * "request_route_find" to say the route for the current request has been
+     * found by us.
+     * 
+     * @return void
+     */
+    protected function sendNotifyRouteFindToOthers()
+    {
+        $app = \BFW\Application::getInstance();
+        $app->notifyAction('request_route_find');
     }
 }
